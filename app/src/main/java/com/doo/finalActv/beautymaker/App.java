ee26cf30acee9f0ad1ecfd7bd0f7a60c9079d80a
Juber <>
2025-07-08 18:32:31 -0400

    adjust: start app maximized

--- app/src/main/java/com/doo/finalActv/beautymaker/App.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/App.java
@@ -1,11 +1,13 @@
 package com.doo.finalActv.beautymaker;
 
 import com.doo.finalActv.beautymaker.ui.MainWindow;
+import javax.swing.JFrame;
 
 public class App {
 
     public static void main(String[] args) {
       MainWindow mainWindow = new MainWindow();
+      mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
       mainWindow.setVisible(true);
     }
 }
