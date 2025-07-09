c7f8e3602b0b819dfc766187601aa529f1cba7e7
Juber <>
2025-07-09 14:53:14 -0400

    improve NotificationPanel
    - add constructors with configurable display-time parameter
    - make display-time and type optional, falling back to default values when omitted

--- app/src/main/java/com/doo/finalActv/beautymaker/ui/customComponents/NotificationPanel.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/ui/customComponents/NotificationPanel.java
@@ -12,15 +12,18 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class NotificationPanel extends JPanel {
-
   public static final int WIDTH = 300;
   public static final int HEIGHT = 100;
-  private static int DISPLAY_TIME = 5000;
+  private static final int STD_DISPLAY_TIME = 5000;
+  private static final NotificationType STD_TYPE = NotificationType.WARNING;
 
   private JPanel container;
+  private int displayTime;
+  Timer timer;
 
-  public NotificationPanel(String title, String message, JPanel mainContainer, NotificationType type) {
+  public NotificationPanel(String title, String message, JPanel mainContainer, int display_time, NotificationType type) {
     this.container = mainContainer;
+    this.displayTime = display_time > 0 ? display_time : STD_DISPLAY_TIME;
 
     setLayout(new BorderLayout());
     setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -45,14 +48,26 @@ public class NotificationPanel extends JPanel {
     startLifecycle();
   }
 
+  public NotificationPanel(String title, String message, JPanel mainContainer) {
+    this(title, message, mainContainer, STD_DISPLAY_TIME, STD_TYPE);
+  }
+
+  public NotificationPanel(String title, String message, JPanel mainContainer, NotificationType type) {
+    this(title, message, mainContainer, STD_DISPLAY_TIME, type);
+  }
+
+  public NotificationPanel(String title, String message, JPanel mainContainer, int display_time) {
+    this(title, message, mainContainer, display_time, STD_TYPE);
+  }
+
   private void startLifecycle() {
-    Timer timer = new Timer(DISPLAY_TIME, (ActionEvent e) -> dispose());
+    this.timer = new Timer(this.displayTime, (ActionEvent e) -> dispose());
     timer.setRepeats(false);
     timer.start();
   }
 
   private void dispose() {
-    if (container != null) {
+    if (this.container != null) {
       container.remove(this);
       container.revalidate();
       container.repaint();
