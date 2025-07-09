2f24d0d77276d21191bc5c987ae5b633bc01ddf7
Juber <>
2025-07-08 23:22:01 -0400

    test: add temporary test for notification system

--- app/src/main/java/com/doo/finalActv/beautymaker/App.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/App.java
@@ -1,6 +1,8 @@
 package com.doo.finalActv.beautymaker;
 
 import com.doo.finalActv.beautymaker.model.NotificationType;
+import com.doo.finalActv.beautymaker.serivce.event.EventManager;
+import com.doo.finalActv.beautymaker.serivce.event.model.NotificationEvent;
 import com.doo.finalActv.beautymaker.ui.MainWindow;
 import javax.swing.JFrame;
 
@@ -10,5 +12,34 @@ public class App {
     MainWindow mainWindow = new MainWindow();
     mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
     mainWindow.setVisible(true);
+
+    // Test the notification system
+    testNotification(mainWindow);
+
+  }
+
+  private static void testNotification(MainWindow mainWindow) {
+    try {
+      EventManager.getInstance().publish(new NotificationEvent(
+          NotificationType.WARNING,
+          "Test Notification",
+          "This is a test notification message.\nIt can span multiple lines."
+      ));
+      Thread.sleep(2000);
+      EventManager.getInstance().publish(new NotificationEvent(
+          NotificationType.WARNING,
+          "Another Notification",
+          "This is another test notification message."
+      ));
+      Thread.sleep(2000);
+      // a large text message notification
+      EventManager.getInstance().publish(new NotificationEvent(
+          NotificationType.ERROR,
+          "Large Notification",
+          "This is a large notification message that is intended to test the wrapping and display of long text in the notification panel. It should handle multiple lines correctly and not overflow the panel boundaries. Let's see how it performs with this lengthy message."
+      ));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
