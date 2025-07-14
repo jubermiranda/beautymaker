f4822975e26ee9f53f8b7338e988185e11edf5d8
Juber <>
2025-07-14 04:15:55 -0400

    feat: initial staff view
    - add container to store staff cards
    - add some test data to see how ui show cards
    - (next step: fetch staff from db)

--- app/src/main/java/com/doo/finalActv/beautymaker/ui/client/StaffView.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/ui/client/StaffView.java
@@ -1,21 +1,15 @@
 package com.doo.finalActv.beautymaker.ui.client;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
+import java.time.LocalDate;
+import javax.swing.Box;
 
-/**
- *
- * @author user
- */
 public class StaffView extends javax.swing.JPanel {
 
-  /**
-   * Creates new form StaffView
-   */
+
   public StaffView() {
     initComponents();
+    
+    this.initialize();
   }
 
   /**
@@ -28,28 +22,57 @@ public class StaffView extends javax.swing.JPanel {
   private void initComponents() {
 
     jLabel1 = new javax.swing.JLabel();
+    jScrollPane1 = new javax.swing.JScrollPane();
+    staffContainerPanel = new javax.swing.JPanel();
 
+    jLabel1.setFont(new java.awt.Font("Cantarell", 1, 15)); // NOI18N
     jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-    jLabel1.setText("Hello! this is staff view");
+    jLabel1.setText("Beauty Agents");
+
+    staffContainerPanel.setLayout(new javax.swing.BoxLayout(staffContainerPanel, javax.swing.BoxLayout.Y_AXIS));
+    jScrollPane1.setViewportView(staffContainerPanel);
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
     this.setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(layout.createSequentialGroup()
-        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 476, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(0, 524, Short.MAX_VALUE))
+        .addContainerGap()
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 988, Short.MAX_VALUE)
+          .addComponent(jScrollPane1))
+        .addContainerGap())
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(layout.createSequentialGroup()
-        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addGap(0, 731, Short.MAX_VALUE))
+        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 882, Short.MAX_VALUE)
+        .addContainerGap())
     );
   }// </editor-fold>//GEN-END:initComponents
 
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JLabel jLabel1;
+  private javax.swing.JScrollPane jScrollPane1;
+  private javax.swing.JPanel staffContainerPanel;
   // End of variables declaration//GEN-END:variables
+
+  private void initialize() {
+    staffContainerPanel.add(Box.createVerticalStrut(20));
+    // create some test StaffCards
+    for(int i = 0; i < 10; i++) {
+      String name = "Staff " + (i + 1);
+      float rating = (float) (Math.random() * 5);
+      LocalDate hireDate = LocalDate.now().minusDays((long) (Math.random() * 365* 15));
+
+      StaffCardView staffCard = new StaffCardView(name, rating, hireDate);
+      staffContainerPanel.add(staffCard);
+      staffContainerPanel.add(Box.createVerticalStrut(20));
+      staffContainerPanel.revalidate();
+    }
+    this.repaint();
+  }
 }
