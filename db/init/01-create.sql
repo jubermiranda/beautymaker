ebcbc142ffa5502f2b02b990c8ce6cee16168861
Juber <>
2025-07-14 00:48:02 -0400

    update schema: employees
    - add table to represent employee rating
    - add function to get employee rating
    - add hire-date atribute to employee

--- db/init/01-create.sql
+++ db/init/01-create.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS beautymaker.clients (
 
 CREATE TABLE IF NOT EXISTS beautymaker.employees (
   user_id INTEGER PRIMARY KEY REFERENCES beautymaker.users(id) ON DELETE CASCADE
+  hire_date DATE NOT NULL DEFAULT CURRENT_DATE,
 );
 -- function to get user type based on user_id
 CREATE OR REPLACE FUNCTION beautymaker.get_user_type(user_id_param INT)
@@ -35,4 +36,32 @@ END;
 $$ LANGUAGE plpgsql;
 ------------------
 
+-- employee ratings
+CREATE TABLE IF NOT EXISTS beautymaker.employee_ratings (
+  id SERIAL PRIMARY KEY,
+  employee_id INTEGER NOT NULL REFERENCES beautymaker.employees(user_id) ON DELETE CASCADE,
+  client_id INTEGER NOT NULL REFERENCES beautymaker.clients(user_id) ON DELETE CASCADE,
+  rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+  comment VARCHAR(500),
+  -- TODO: appointment_id INTEGER NOT NULL REFERENCES beautymaker.appointments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
 
+-- employee rating function
+-- Its returns the average rating of an employee based on their ratings.
+-- or returns -1 if there are no ratings for the employee.
+CREATE OR REPLACE FUNCTION beautymaker.get_employee_rating(employee_id_param INT)
+RETURNS FLOAT AS $$
+DECLARE
+    avg_rating FLOAT;
+BEGIN
+    SELECT AVG(rating) INTO avg_rating
+    FROM beautymaker.employee_ratings
+    WHERE employee_id = employee_id_param;
+
+    IF avg_rating IS NULL THEN
+        RETURN -1;
+    ELSE
+        RETURN avg_rating;
+    END IF;
+END;
