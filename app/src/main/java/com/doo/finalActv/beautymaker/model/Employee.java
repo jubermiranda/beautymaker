72f10c494890d7ccdb1a382ee5b0fb9c46f7e2ae
Juber <>
2025-07-14 00:49:32 -0400

    update Employee model to match new database schema

--- app/src/main/java/com/doo/finalActv/beautymaker/model/Employee.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/model/Employee.java
@@ -3,8 +3,10 @@ package com.doo.finalActv.beautymaker.model;
 import java.time.LocalDate;
 
 public class Employee extends User {
+  private LocalDate hireDate;
 
-  public Employee(int id, String name, String email, LocalDate birthDate) {
+  public Employee(int id, String name, String email, LocalDate birthDate, LocalDate hireDate) {
     super(id, name, email, birthDate);
+    this.hireDate = hireDate;
   }
 }
