21561e5b071b16c19b03d48141010180d5edd2c9
Juber <>
2025-07-20 06:47:49 -0400

    update appointment event
    - add generic atribute ( extends `AppointmentElementSelectedEvent`) to represent the selected item

--- app/src/main/java/com/doo/finalActv/beautymaker/serivce/event/model/AppointmentElementSelectedEvent.java
+++ app/src/main/java/com/doo/finalActv/beautymaker/serivce/event/model/AppointmentElementSelectedEvent.java
@@ -1,5 +1,9 @@
 package com.doo.finalActv.beautymaker.serivce.event.model;
 
-public class AppointmentElementSelectedEvent {
-  
+public class AppointmentElementSelectedEvent<T extends AppointmentElementSelectedEvent> {
+  public final T selectedElement;
+
+  public AppointmentElementSelectedEvent(T selectedElement) {
+    this.selectedElement = selectedElement;
+  }
 }
